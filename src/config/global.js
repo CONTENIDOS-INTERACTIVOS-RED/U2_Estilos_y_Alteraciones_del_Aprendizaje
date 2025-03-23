3528a0e633dc0516b4760df8da70220adcc9bd86
Francisco Lizcano <flizcanor@icloud.com>
2025-03-23 10:44:31 -0500

    Update global.js

--- src/config/global.js
+++ src/config/global.js
@@ -118,26 +118,170 @@ export default {
   referencias: [
     {
       referencia:
-        'Ardaraz, M. (2002). Teoría de las tres dimensiones de desarrollo sostenible: . Ecosistemas, 11(2)',
+        'Artigas, J., Rigau, E., & García-Nonell, K. (2008). Trastornos del lenguaje. AEP: Protocolos de actualización, 24, 178-84.',
       link:
-        'https://www.revistaecosistemas.net/index.php/ecosistemas/article/view/614',
+        'https://cnimaresme.com/publicaciones/Trastornos%20de%20lenguaje.pdf',
     },
     {
       referencia:
-        'Beltrán, D. J. (2002). La estrategia de desarrollo sostenible de la Unión Europea en el contexto global: de Río a Johannesburgo. ICE. Revista de Economía, (800).',
-      link: '',
+        'Barcos, V. (17 de abril, 2023). Niños con discapacidad visual en el aula ¿Cómo actuar en la etapa infantil? Canal Educación y Sociedad.',
+      link:
+        'https://www.inesem.es/revistadigital/educacion-sociedad/discapacidad-visual-alumno/',
+    },
+    {
+      referencia:
+        'Chappotin, D. (27 de abril 2022). Trastornos del neurodesarrollo: concepto, tipos y tratamiento. Neuronup.com.',
+      link:
+        'https://neuronup.com/estimulacion-y-rehabilitacion-cognitiva/trastornos-del-neurodesarrollo/trastornos-del-neurodesarrollo-concepto-tipos-y-tratamiento/?form=MG0AV3',
+    },
+    {
+      referencia: 'DECRETO 1421 DE 2017. (s/f). Gov.co.',
+      link:
+        'https://www.suin-juriscol.gov.co/viewDocument.asp?ruta=Decretos/30033428',
+    },
+    {
+      referencia:
+        'Escuela de Postgrado de Psicología y Psiquiatría. (26 de noviembre, 2021) Trastornos del Lenguaje y Comunicación.',
+      link:
+        'https://eepsicologia.com/trastornos-del-lenguaje-comunicacion/?form=MG0AV3',
+    },
+    {
+      referencia:
+        'Hernández-Quirama, A., & colaboradores. (2019). Configuración histórica de la discapacidad visual y sus implicaciones para la salud pública.',
+      link:
+        'https://www.redalyc.org/journal/3438/343862451008/343862451008.pdf',
+    },
+    {
+      referencia:
+        'Jones, B.k. (junio, 2023). Trastorno de procesamiento sensorial (SPD). familydoctor.org.',
+      link:
+        'https://es.familydoctor.org/condicion/trastorno-de-procesamiento-sensorial-tps-es/',
+    },
+    {
+      referencia:
+        'Luque Rojas, M. J. & Luque Parra, D. J. (2016). Discapacidad intelectual: consideraciones para su intervención psicoeducativa: (ed.). Wanceulen Editorial.',
+      link: 'https://elibro.net/es/ereader/tecnologicadeloriente/63444?page=26',
+    },
+    {
+      referencia:
+        'Martos Pérez, J. Llorente Comí, M. & González Navarro, A. (2020). Los niños pequeños con Autismo: ( ed.). Editorial CEPE.',
+      link: 'https://elibro.net/es/ereader/tecnologicadeloriente/15354?page=21',
+    },
+    {
+      referencia:
+        'Mitjana, L. R. (27 de junio, 2021). Los 3 grados del trastorno del espectro autista. Muy Salud.',
+      link:
+        'https://muysalud.com/enfermedades/grados-trastorno-espectro-autista/',
+    },
+    {
+      referencia:
+        'Ministerio de Educación Nacional de Colombia. (2006). Orientaciones pedagógicas para la atención educativa a estudiantes con autismo.',
+      link:
+        'https://fundacionintegrar.org/wp-content/uploads/2013/07/orientacionespedagogicasministeriodeeducacioncolombia.pdf',
+    },
+    {
+      referencia:
+        'Moreno-Flagge, N. (2013). Trastornos del lenguaje. Diagnóstico y tratamiento. Psyciencia.com.',
+      link:
+        'https://pavlov.psyciencia.com/2013/11/Trastornos-del-lenguaje-diagnostico-y-tratamiento.pdf',
+    },
+    {
+      referencia:
+        'Ocronos, R. (1 de marzo, 2021). Trastorno por déficit de atención con hiperactividad (TDAH) en niños. Revisión bibliográfica. Ocronos - Editorial Científico-Técnica.',
+      link: 'https://revistamedica.com/deficit-atencion-hiperactividad/',
+    },
+    {
+      referencia:
+        'Organización Mundial de la Salud. (2023). Ceguera y discapacidad visual.',
+      link:
+        'https://www.who.int/es/news-room/fact-sheets/detail/blindness-and-visual-impairment',
+    },
+    {
+      referencia:
+        'Pérez Sánchez, A. M. (2013). TDI. Trastornos del Desarrollo Intelectual (apuntes).',
+      link:
+        'https://rua.ua.es/dspace/bitstream/10045/33196/1/TDI.%20Trastornos%20del%20Desarrollo%20Intelectual%20(apuntes).pdf',
+    },
+    {
+      referencia:
+        'Pizarro, M., Saffery, K., & Gajardo, P. (2022). Trastorno del procesamiento sensorial. Una mirada conjunta desde la terapia ocupacional y la otorrinolaringología. Revista de otorrinolaringología y cirugía de cabeza y cuello, 82(1), 114-126.',
+      link:
+        'https://www.scielo.cl/scielo.php?pid=S0718-48162022000100114&script=sci_arttext',
+    },
+    {
+      referencia:
+        'Ricardo (4 de noviembre, 2020). ¿Qué es la discapacidad visual? - Definición, causas y síntomas. Estudyando.',
+      link:
+        'https://estudyando.com/que-es-la-discapacidad-visual-definicion-causas-y-sintomas/',
+    },
+    {
+      referencia:
+        'Rupérez, A., Ramos, I., Machado, I., Fernández, D., Gortázar, M., & Aguilera, S. (2022). Trastornos del lenguaje, del habla y de la comunicación. Conceptos, clasificación y clínica. Protoc dign ter pediatr, 1(1), 19-30.',
+      link:
+        'https://www.aeped.es/sites/default/files/documentos/03.pdf?form=MG0AV3',
+    },
+    {
+      referencia:
+        'Sulkes, S. B. (s/f). Definición de los trastornos del desarrollo. Manual MSD versión para público general.',
+      link:
+        'https://www.msdmanuals.com/es/hogar/salud-infantil/trastornos-del-aprendizaje-y-del-desarrollo/definici%C3%B3n-de-los-trastornos-del-desarrollo',
+    },
+    {
+      referencia:
+        'Torres, A. (2 de octubre 2018). Ceguera (discapacidad visual): qué es, tipos, causas y tratamiento. pymOrganization.',
+      link: 'https://psicologiaynente.com/salud/ceguera',
     },
   ],
   glosario: [
     {
-      termino: 'Bioética',
+      termino: 'Accesibilidad',
       significado:
-        'disciplina que estudia las implicaciones éticas, sociales y legales de los avances biológicos y médicos, buscando garantizar que las decisiones en estos campos respeten la dignidad humana, los derechos fundamentales y la justicia social.',
+        'Facilidad con la que una persona con discapacidad puede acceder a un lugar, servicio o información.',
     },
     {
-      termino: '<em>Design Thinking</em>',
+      termino: 'Adaptación',
       significado:
-        'método de diseño centrado en el usuario que se basa en la comprensión de las necesidades y expectativas de los clientes, para crear soluciones innovadoras y efectivas.',
+        'Modificación de un entorno o de una actividad para que una persona con discapacidad pueda participar de manera efectiva.',
+    },
+    {
+      termino: 'Ataxia',
+      significado:
+        'Grupo de trastornos que afectan la coordinación de los movimientos, lo que puede causar dificultad para caminar, hablar y realizar otras actividades.',
+    },
+    {
+      termino: 'Condrodistrofia',
+      significado:
+        'Grupo de trastornos genéticos que afectan el crecimiento de los cartílagos y huesos, provocando enanismo desproporcionado y otras características físicas distintivas.',
+    },
+    {
+      termino: 'Discapacidad intelectual',
+      significado:
+        'Limitación significativa en las funciones intelectuales y en las habilidades de adaptación que se manifiestan antes de los 18 años.',
+    },
+    {
+      termino: 'Diversidad funcional',
+      significado:
+        'Reconocimiento de la diversidad de capacidades humanas y la necesidad de crear entornos inclusivos para todos.',
+    },
+    {
+      termino: 'Habilitación',
+      significado:
+        'Proceso de desarrollar habilidades en una persona con discapacidad para aumentar su independencia y calidad de vida.',
+    },
+    {
+      termino: 'Inclusión',
+      significado:
+        'Proceso de integrar a todas las personas, independientemente de sus capacidades, en todos los aspectos de la sociedad.',
+    },
+    {
+      termino: 'Rehabilitación',
+      significado:
+        'Proceso de restaurar o mejorar las funciones perdidas o dañadas debido a una discapacidad.',
+    },
+    {
+      termino: 'Tecnología asistida',
+      significado:
+        'Herramientas y dispositivos que ayudan a las personas con discapacidad a realizar tareas y actividades de la vida diaria.',
     },
   ],
 }
