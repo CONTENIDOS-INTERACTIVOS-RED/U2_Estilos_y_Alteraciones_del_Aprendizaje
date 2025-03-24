56a3be6edc5faed08f80394c7ebbdeb245d8bc29
Francisco Lizcano <flizcanor@icloud.com>
2025-03-24 00:48:09 -0500

    Update Tema3.vue

--- src/views/curso/Tema3.vue
+++ src/views/curso/Tema3.vue
@@ -483,6 +483,13 @@
 
       p(data-aos="fade-up").mb-5 Los trastornos del desarrollo sensorial motórico afectan la capacidad de una persona para procesar estímulos sensoriales y responder a ellos con movimientos adecuados. Estos trastornos pueden impactar el equilibrio, la coordinación, la percepción del espacio y la integración de la información sensorial con la motricidad.
 
+      
+
+      .bg-full-width.border-top.actividad.bg-color-actividad
+        .p-4.p-md-5
+          #Actividad                
+            <Actividad :cuestionario="cuestionario"/>
+
       .bg-full-width.border-top.color-primario
         .p-4.p-md-5
           h2(data-aos="fade-left") MATERIAL COMPLEMENTARIO
@@ -502,35 +509,6 @@
                 img.me-3(src='@/assets/template/icono-yt.svg' :style="{'max-width':'16px'}")
                 a(href="https://www.youtube.com/watch?v=zRM3sVJYqbk" target="_blank" rel="noopener noreferrer") CanalFenascol. (2023, 11 de octubre). La Lengua de Señas es un derecho y en Colombia la Ley 324 de 1996 lo reconoce. Youtube.
             
-            .col-12.col-md-6.col-lg-3.offset-lg-1
-              figure
-                img(src='@/assets/componentes/material-complementario.svg', alt='')
-      
-
-      .bg-full-width.border-top.actividad.bg-color-actividad
-        .p-4.p-md-5
-          #Actividad                
-            <Actividad :cuestionario="cuestionario"/>
-
-      .bg-full-width.border-top.color-primario
-        .p-4.p-md-5
-          h2(data-aos="fade-left") MATERIAL COMPLEMENTARIO
-          .row.material-complementario
-            .col-12.col-md-6.col-lg-7
-              p Los invitamos a explorar el material complementario de este curso, en esta sección encontrará recursos que le permitirán profundizar  y enriquecer su aprendizaje en los temas tratados en esta unidad.
-  
-              p.d-flex.my-4
-                img.me-3(src='@/assets/componentes/link.svg' :style="{'max-width':'16px'}")
-                a(href="https://elibro.net/es/lc/tecnologicadeloriente/titulos/274611" target="_blank" rel="noopener noreferrer") De Castro Emilio Cerdá, F. J. A. L. M. (s/f). Las energías renovables en el ámbito internacional. Revistasice.com. 
-  
-              p.d-flex.my-4
-                img.me-3(src='@/assets/template/book.svg' :style="{'max-width':'16px'}")
-                span De Janeiro, R. (1992). La declaración del Río sobre el Medio Ambiente y el Desarrollo. Río de Janeiro República Federativa del Brasil., aprobada en Estocolmo el, 16.
-  
-              p.d-flex.my-4
-                img.me-3(src='@/assets/template/icono-yt.svg' :style="{'max-width':'16px'}")
-                a(href="https://youtu.be/QKLjwCMyv3M?si=uAWhO9tnyghljaJg" target="_blank" rel="noopener noreferrer") Innovación y sostenibilidad.
-  
             .col-12.col-md-6.col-lg-3.offset-lg-1
               figure
                 img(src='@/assets/componentes/material-complementario.svg', alt='')
